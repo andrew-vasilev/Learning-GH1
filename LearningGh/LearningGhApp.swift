50948260a5105f066a464793d824b2ac5f3fbb4f
Standrewss <avis.andrew25@gmail.com>
2023-12-01 09:55:14 +0700

    Initial Commit

---
+++ LearningGh/LearningGhApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGhApp.swift
+//  LearningGh
+//
+//  Created by Андрей Васильев on 01.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct LearningGhApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
